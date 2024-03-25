7d4b0a43941260376359fa7bac14250d7f776469
Stas <ovcharenko.s.a.1995@gmail.com>
2024-03-25 17:28:02 +0200

    enable binding and refactored cod with binding and with

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ActionMenuView
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -10,35 +11,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.example.calculator.databinding.ActivityMainBinding
 import com.ezylang.evalex.Expression
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) = with(binding) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val zeroButton: Button = findViewById(R.id.zeroButton)
-        val oneButton: Button = findViewById(R.id.oneButton)
-        val twoButton: Button = findViewById(R.id.twoButton)
-        val threeButton: Button = findViewById(R.id.threeButton)
-        val fourButton: Button = findViewById(R.id.fourButton)
-        val fiveButton: Button = findViewById(R.id.fiveButton)
-        val sixButton: Button = findViewById(R.id.sixButton)
-        val sevenButton: Button = findViewById(R.id.sevenButton)
-        val eightButton: Button = findViewById(R.id.eightButton)
-        val nineButton: Button = findViewById(R.id.nineButton)
-
-        val equalButton: Button = findViewById(R.id.equalButton)
-        val pointButton: Button = findViewById(R.id.pointButton)
-        val plusButton: Button = findViewById(R.id.plusButton)
-        val minusButton: Button = findViewById(R.id.minusButton)
-        val multiplyButton: Button = findViewById(R.id.multiplyButton)
-        val divideButton: Button = findViewById(R.id.divideButton)
-        val percentButton: Button = findViewById(R.id.percentButton)
-        val backSpaceButton: Button = findViewById(R.id.backSpaceButton)
-        val deleteAllButton: Button = findViewById(R.id.deleteAllButton)
-
-        val resultTextView: TextView = findViewById(R.id.resultTextView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val numberStringBuilder: StringBuilder = StringBuilder()
 
@@ -123,7 +106,7 @@ class MainActivity : AppCompatActivity() {
             resultTextView.text = numberStringBuilder
         }
 
-        backSpaceButton.setOnClickListener {
+       backSpaceButton.setOnClickListener {
             val lastIndex: Int = numberStringBuilder.indices.last
 
             if (lastIndex >= 0) {
@@ -148,7 +131,7 @@ class MainActivity : AppCompatActivity() {
                 numberStringBuilder.isEmpty()
             } else {
                 try {
-                    val expression: Expression = Expression(numberStringBuilder.toString())
+                    val expression = Expression(numberStringBuilder.toString())
                     val expressionResult = expression.evaluate().stringValue
 
 
